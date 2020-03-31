442a0f8c6d5889f1386c00f1d465e7585a151c84
RAKIBUL ISLAM <irakibul550@gmail.com>
2020-03-31 23:08:50 +0800

    Add files via upload

---
+++ lib/app/home/entries/entry_job.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_app/app/home/models/entry.dart';
+import 'package:flutter_app/app/home/models/job.dart';
+
+class EntryJob {
+  EntryJob(this.entry, this.job);
+
+  final Entry entry;
+  final Job job;
+}
